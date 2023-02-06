5f125116ff4850137d46914b3d20bc2558ed8068
nk <badhon7574@gmail.com>
2023-02-06 12:06:12 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react"
+
+export default function counter(){
+
+useReducer(reducer, initialState)
+return(
+  <div>
+<div>count - 0 </div>
+<button type="button">Increment</button>
+<button type="button">Decrement</button>
+
+  </div>
+)
+
+}
